e71ff6553912d8c307db55e87376fcb44f9e93d0
ali <alimohammadi7117@gmail.com>
2019-02-03 17:30:17 +0330

    add loading view

--- lib/datasource/movie_data_source.dart
+++ lib/datasource/movie_data_source.dart
@@ -7,8 +7,8 @@ class MovieDataSource{
   bool hasMoreData = false;
   final MovieRepository repository;
 
-  final _loadAfterStateSubject = BehaviorSubject<bool>(seedValue: false);
-  Observable<bool> get networkState => _loadAfterStateSubject.stream;
+  final _loadAfterStateSubject = BehaviorSubject<Object>();
+  Observable<Object> get networkState => _loadAfterStateSubject.stream;
 
   var _movieList = List<Movie>();
   final _moviesSubject = BehaviorSubject<List<Movie>>();
@@ -30,8 +30,9 @@ class MovieDataSource{
   }
 
   loadAfter() async{
+    _loadAfterStateSubject.add(null);
     var movieList = await repository.getMovieList(page).catchError((error){
-      _loadAfterStateSubject.addError(error);
+      _loadAfterStateSubject.add(error);
       return;
     });
     var currentpage = int.tryParse(movieList.metaData.currentPage);
