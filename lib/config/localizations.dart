c83b971100568630635f953b28d62927c033d763
ali <alimohammadi7117@gmail.com>
2019-02-04 10:51:31 +0330

    add multi lang support

---
+++ lib/config/localizations.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:moviesapi_flutter/config/l10n/messages_all.dart';
+
+class AppLocalizations{
+
+
+  static Future<AppLocalizations> load(Locale locale) {
+
+    final String name =
+    locale.countryCode == null ? locale.languageCode : locale.toString();
+    final String localeName = Intl.canonicalizedLocale(name);
+
+    return initializeMessages(localeName).then((bool _) {
+      Intl.defaultLocale = localeName;
+      return new AppLocalizations();
+    });
+
+  }
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+
+  String get title {
+    return Intl.message('Hello world App',
+        name: 'title', desc: 'The application title');
+  }
+
+  String get hello {
+    return Intl.message('Hello', name: 'hello');
+  }
+
+  String get newMovie => Intl.message("New Movie",name: "newMovie");
+  String get movieApi => Intl.message("Movie Api",name: "movieApi");
+}
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations>{
+  @override
+  bool isSupported(Locale locale) {
+    return ['fa', 'en'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return AppLocalizations.load(locale);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) {
+    return false;
+  }
+
+}
